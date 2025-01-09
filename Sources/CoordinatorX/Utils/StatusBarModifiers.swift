9666912257994fb8edace032e3b923ee7dfecba4
Eugene Don <edon2005@gmail.com>
2025-01-09 14:20:54 +0100

    [014] Add modifier .navStatusBar(hidden:) to control Status Bar from View inside Navigation Stack.

---
+++ Sources/CoordinatorX/Utils/StatusBarModifiers.swift
@@ -0,0 +1,29 @@
+//
+//  StatusBarModifiers.swift
+//  CoordinatorX
+//
+//  Created by Yevhen Don on 09/01/2025.
+//
+
+import SwiftUI
+
+struct StatusBar: ViewModifier {
+
+    let isHidden: Bool
+
+    init(isHidden: Bool) {
+        self.isHidden = isHidden
+    }
+
+    func body(content: Content) -> some View {
+        let _ = statusBarHiddenSubject.send(isHidden)
+        content
+    }
+}
+
+extension View {
+    public func navStatusBar(isHidden: Bool) -> some View {
+        let statusBar = StatusBar(isHidden: isHidden)
+        return modifier(statusBar)
+    }
+}
